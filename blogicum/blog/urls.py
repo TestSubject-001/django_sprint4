6475f72e1c34552e16dbc49918016fcd49e4b449
bruh <new@example.com>
2026-03-17 16:30:33 +0300

    GG EZ!

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -22,6 +22,6 @@ urlpatterns = [
         name='delete_comment'
     ),
     path('category/<slug:category_slug>/', views.category_posts, name='category_posts'),
-    path('profile/<str:username>/', views.profile, name='profile'),
     path('profile/edit/', views.edit_profile, name='edit_profile'),
+    path('profile/<str:username>/', views.profile, name='profile'),
 ]
